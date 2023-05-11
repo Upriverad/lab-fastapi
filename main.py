ea735d261776c6ebfdf1fa66babac6cd192a05ad
Upriverad <s222263@student.uek.krakow.pl>
2023-05-11 09:07:30 +0200

    elo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
+@app.post("/students/")
+async def create_student(student: StudentCreateSchema):
+    return student
